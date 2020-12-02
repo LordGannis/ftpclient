4df6c50fbb638436764c6cf3086c158d70e4493d
Guilherme <guilherme.castpartner@gmail.com>
2020-12-02 09:24:01 +0100

    Changes

--- src/com/test/ftp/FtpClient.java
+++ src/com/test/ftp/FtpClient.java
@@ -1,27 +1,31 @@
 package com.test.ftp;
 
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPSClient;
-
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.commons.net.ftp.FTPSClient;
+
 public class FtpClient {
 
     public static void main(String[] args) {
         try {
-            FTPClient ftpClient = new FTPSClient("TLSv1.2");
+        	FTPSClient ftpClient = new FTPSClient("TLSv1.2");
+            System.out.println("Created the FTPSClient");
 
             ftpClient.connect(args[0], Integer.parseInt(args[1]));
-            ftpClient.login(args[2], args[3]);
+            System.out.println("Connected successfully to " + (args[0].concat(":").concat(args[1])));
+            boolean login = ftpClient.login(args[2], args[3]);
+            System.out.println("Login successfully: " + login);
 
             File file = new File(args[4]);
             InputStream targetStream = new DataInputStream(new FileInputStream(file));
 
+            System.out.println("Sending file: " + args[4]);
             if(!ftpClient.storeFile(args[5], targetStream)) {
+                System.out.println("File sent");
                 System.out.println("Reply: " + ftpClient.getReply());
                 System.out.println("Code: " + ftpClient.getReplyCode());
                 System.out.println("Reply text: " + ftpClient.getReplyString());
